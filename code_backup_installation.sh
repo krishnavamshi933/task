f11642c27cc8eb42fc7a14df5cb7d9dcb269493a
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2023-06-08 08:41:50 +0530

    Update code_backup_installation.sh

--- code_backup_installation.sh
+++ code_backup_installation.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+
+OLD_CODE_DIR="/home/user/oldcode"
+NEW_CODE_DIR="/home/user/newcode"
+
 # Backup old code
-if [ -d /path/to/oldcode ]; then
-    tar -czvf /path/to/backup.tar.gz #/path/to/oldcode
+if [ -d "$OLD_CODE_DIR" ]; then
+    tar -czvf /path/to/backup.tar.gz "$OLD_CODE_DIR"
 fi
-git clone https://github.com/your-git-repo.git #/path/to/newcode
-cd #/path/to/newcode
+
+# Install latest code from Git repository
+git clone https://github.com/your-git-repo.git "$NEW_CODE_DIR"
+cd "$NEW_CODE_DIR"
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Perform migrations or setup steps
 python manage.py migrate
