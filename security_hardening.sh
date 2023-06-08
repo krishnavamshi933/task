1dc826694f4ef8c64de90afaea311ee994da4fa4
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2023-06-08 09:23:54 +0530

    Update security_hardening.sh

--- security_hardening.sh
+++ security_hardening.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-apt update
-apt upgrade -y
-ufw default deny incoming
-ufw default allow outgoing
-ufw allow ssh
-ufw enable
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-systemctl restart sshd
-apt install fail2ban -y
-apt install rsyslog -y
-systemctl enable rsyslog
+
+# Update system packages
+sudo apt update
+sudo apt upgrade -y
+
+# Disable root login
+sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+sudo service ssh restart
+
+# Enable firewall and allow necessary services
+sudo ufw enable
+sudo ufw allow OpenSSH
+sudo ufw allow 80  # Allow HTTP traffic
+sudo ufw allow 443  # Allow HTTPS traffic
+sudo ufw reload
+
+# Install and configure fail2ban
+sudo apt install -y fail2ban
+sudo systemctl enable fail2ban
+sudo systemctl start fail2ban
