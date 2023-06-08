fed9caa16de5df87e78aabc11e56429e1610975c
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2023-06-08 08:58:38 +0530

    Update code_backup_installation.sh

--- code_backup_installation.sh
+++ code_backup_installation.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-OLD_CODE_DIR="/home/user/oldcode"
-NEW_CODE_DIR="/home/user/newcode"
+HOME_PATH="/home/ec2-user"
+OLD_CODE_DIR="$HOME_PATH/oldcode"
+NEW_CODE_DIR="$HOME_PATH/newcode"
+BACKUP_PATH="$HOME_PATH/backup.tar.gz"
 
 # Backup old code
 if [ -d "$OLD_CODE_DIR" ]; then
-    tar -czvf /path/to/backup.tar.gz "$OLD_CODE_DIR"
+    tar -czvf "$BACKUP_PATH" "$OLD_CODE_DIR"
 fi
 
 # Install latest code from Git repository
-git clone https://github.com/your-git-repo.git "$NEW_CODE_DIR"
+git clone https://github.com/krishnavamshi933/task.git "$NEW_CODE_DIR"
 cd "$NEW_CODE_DIR"
 
 # Install dependencies
