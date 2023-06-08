ee7b180b083b225894b5928f8da6b8ff4c2e388e
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2023-06-08 08:19:01 +0530

    Update code_backup_installation.sh

--- code_backup_installation.sh
+++ code_backup_installation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Backup old code
 if [ -d /path/to/oldcode ]; then
-    tar -czvf /path/to/backup.tar.gz /path/to/oldcode
+    tar -czvf /path/to/backup.tar.gz #/path/to/oldcode
 fi
-git clone https://github.com/your-git-repo.git /path/to/newcode
-cd /path/to/newcode
+git clone https://github.com/your-git-repo.git #/path/to/newcode
+cd #/path/to/newcode
 pip install -r requirements.txt
 python manage.py migrate
